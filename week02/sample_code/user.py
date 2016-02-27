4d6286cdd4b273597243ec4149983f0a9f3fb0ff
Tom Clark <tom.clark@op.ac.nz>
2016-02-27 15:34:04 +1300

    fixed indents on docstrings

--- week02/sample_code/user.py
+++ week02/sample_code/user.py
@@ -1,16 +1,16 @@
 
 
 class User:
-""" A simple user class for demonstrating some principles
-of password handling. Not intended  for serious use.
-"""
+    """ A simple user class for demonstrating some principles
+    of password handling. Not intended  for serious use.
+    """
 
     @staticmethod
     def create(username="", password=""):
-    """ Saves supplied username and password in a text file.
-    returns True on success If either argument is ommitted or
-    is empty, does nothing and returns False.
-    """
+        """ Saves supplied username and password in a text file.
+        returns True on success If either argument is ommitted or
+        is empty, does nothing and returns False.
+        """
         if username != "" and password != "":
             with open("passwords.txt", "a") as f:
                 f.write("{}:{}\n".format(username, password))
@@ -20,9 +20,9 @@ of password handling. Not intended  for serious use.
 
     @staticmethod
     def authenticate(username, password):
-    """ Attempts to authenticate a user with the supplied username and
-    password. Returns True on success, false otherwise.
-    """
+        """ Attempts to authenticate a user with the supplied username and
+        password. Returns True on success, false otherwise.
+        """
         lines = list()
         with open("passwords.txt", "r") as f:
             lines = f.readlines()
